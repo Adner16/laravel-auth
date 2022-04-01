3e09d64d26baf3e4415974ad7fea60009ae7a0e9
Adner16 <giovanni.renda16@gmail.com>
2022-04-01 22:56:47 +0200

    added scropt

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes(['register' => false]);
+
+Route::middleware('auth')
+->prefix('admin')
+->name('admin.')
+->namespace('Admin')
+->group(function(){
+    Route::get('/', 'Admin\HomeController@index')->name('admin.home');
+    Route::resource('posts', 'PostController');
 });
+    
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('{any?}', function () {
+    return view('guest.home');
+})->where("any", ".*");
